40dbb01e5e0869968c5fc5404b14afb292d67bde
kirill pavlov <2020kirillp@gmail.com>
2023-10-03 14:15:14 -0700

    updated styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import "./App.css";
 
 function App() {
 	return (
-		<section class="bg-gray-50 dark:bg-gray-900">
+		<section class="bg-stone-50">
 			<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-				<div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+				<div class="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-amber-200">
 					<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-						<h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+						<h1 class="text-xl font-bold leading-tight tracking-tight text-slate-800 md:text-2xl">
 							Sign in to your account
 						</h1>
 						<form class="space-y-4 md:space-y-6" action="#">
 							<div>
 								<label
 									for="email"
-									class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+									class="block mb-2 text-sm font-medium text-slate-800"
 								>
 									Your email
 								</label>
@@ -21,7 +21,7 @@ function App() {
 									type="email"
 									name="email"
 									id="email"
-									class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									placeholder="name@company.com"
 									required=""
 								/>
@@ -29,7 +29,7 @@ function App() {
 							<div>
 								<label
 									for="password"
-									class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+									class="block mb-2 text-sm font-medium text-slate-800"
 								>
 									Password
 								</label>
@@ -38,7 +38,7 @@ function App() {
 									name="password"
 									id="password"
 									placeholder="••••••••"
-									class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									required=""
 								/>
 							</div>
@@ -54,24 +54,21 @@ function App() {
 										/>
 									</div>
 									<div class="ml-3 text-sm">
-										<label
-											for="remember"
-											class="text-gray-500 dark:text-gray-300"
-										>
+										<label for="remember" class="text-slate-800">
 											Remember me
 										</label>
 									</div>
 								</div>
 								<a
 									href="#"
-									class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+									class="text-sm font-medium text-slate-800 hover:underline"
 								>
 									Forgot password?
 								</a>
 							</div>
 							<button
 								type="submit"
-								class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+								class="btn w-full bg-slate-100 rounded py-2"
 							>
 								Sign in
 							</button>
