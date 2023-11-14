e3a8ff1a3d2d9a6b70f78992885f8be61e1f6cdc
kirill pavlov <2020kirillp@gmail.com>
2023-11-14 12:25:59 -0800

    look at function

---
+++ src/errorTest.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import data from "./json/test2.json";
+
+function errorTest() {
+  data.map(() => {
+    if (data.valid === true) {
+      return data.message;
+    }
+    else if (data.valid === false) {
+      throw new Error("Something wrong with the data");
+    }
+  })
+}
+
+export default errorTest()
