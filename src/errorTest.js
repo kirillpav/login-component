94c4c730f9ae3c579c582d1d3a5ad7f132ccd905
kirill pavlov <2020kirillp@gmail.com>
2023-11-14 12:35:33 -0800

    formatting

--- src/errorTest.js
+++ src/errorTest.js
@@ -3,14 +3,13 @@ import React from "react";
 import data from "./json/test2.json";
 
 function errorTest() {
-  data.map(() => {
-    if (data.valid === true) {
-      return data.message;
-    }
-    else if (data.valid === false) {
-      throw new Error("Something wrong with the data");
-    }
-  })
+    data.map(() => {
+        if (data.valid === true) {
+            return data.message;
+        } else if (data.valid === false) {
+            throw new Error("Something wrong with the data");
+        }
+    })
 }
 
 export default errorTest()
